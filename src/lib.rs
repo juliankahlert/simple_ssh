499138f7f9c4491ddb9dbac9762348567df163be
Julian Kahlert <90937526+juliankahlert@users.noreply.github.com>
2026-01-21 14:48:38 +0100

    src: lib: Increase default inactivity timeout
    Increase the default inactivity_timeout in client::Config from 5
    seconds to 3000 seconds. Apply the new 50 minute timeout in session
    initializations such as connect_noauth and connect_passwd. This reduces
    premature disconnects for longer running sessions and network delays.
    
    Clarify the pty event loop by binding the channel.wait() result to
    msg and matching on Some(ChannelMsg::...) arms. Make Option handling
    explicit and simplify the select branch pattern matching.
    
    Signed-off-by: Julian Kahlert <90937526+juliankahlert@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -374,7 +374,7 @@ impl SessionInner {
     async fn connect_noauth(self) -> Result<Self> {
         if let Self::NoAuth { data, session: _ } = self {
             let config = client::Config {
-                inactivity_timeout: Some(Duration::from_secs(5)),
+                inactivity_timeout: Some(Duration::from_secs(3000)),
                 ..<_>::default()
             };
             let config = Arc::new(config);
@@ -403,7 +403,7 @@ impl SessionInner {
     async fn connect_passwd(self) -> Result<Self> {
         if let Self::Passwd { data, session: _ } = self {
             let config = client::Config {
-                inactivity_timeout: Some(Duration::from_secs(5)),
+                inactivity_timeout: Some(Duration::from_secs(3000)),
                 ..<_>::default()
             };
             let config = Arc::new(config);
@@ -442,7 +442,7 @@ impl SessionInner {
             }
 
             let config = client::Config {
-                inactivity_timeout: Some(Duration::from_secs(5)),
+                inactivity_timeout: Some(Duration::from_secs(3000)),
                 ..<_>::default()
             };
 
@@ -538,20 +538,24 @@ async fn pty(session: &mut client::Handle<Client>, command: &str) -> Result<u32>
                 };
             },
             // There's an event available on the session channel
-            Some(msg) = channel.wait() => {
+            msg = channel.wait() => {
                 match msg {
                     // Write data to the terminal
-                    ChannelMsg::Data { ref data } => {
+                    Some(ChannelMsg::Data { ref data }) => {
                         stdout.write_all(data).await?;
                         stdout.flush().await?;
                     }
                     // The command has returned an exit code
-                    ChannelMsg::ExitStatus { exit_status } => {
+                    Some(ChannelMsg::ExitStatus { exit_status }) => {
                         code = exit_status;
                         if !stdin_closed {
                             channel.eof().await?;
                         }
                         break;
+                    }
+                    // Channel closed unexpectedly
+                    None => {
+                        return Err(Error::msg("Channel closed unexpectedly"));
                     }
                     _ => {}
                 }
